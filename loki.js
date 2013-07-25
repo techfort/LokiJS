06915ee73bf4ed46af548db21595ae87bb49da44
Joe Minichino <joe@joelinux.(none)>
2013-07-25 10:50:03 +0100

    More work on collections

--- loki.js
+++ loki.js
@@ -8,7 +8,7 @@
 
 function trace(message) { 
 	try{
-		console.log(message);
+		if(LokiJS.debugMode) console.log(message);
 	}catch(err){
 		/* no op */
 	}
@@ -18,65 +18,109 @@ var LokiJS = LokiJS || {};
 
 LokiJS = {
 	version : '0.0.1',
-	debugMode : true, 
-	db : function(_name){
+	debugMode : true
+};
+
+function loki(_name){
+	try {
 		var name = _name;
-		if(LokiJS.debugMode) trace('Creating db ' + name);
+		trace('Creating db ' + name);
 
 		var $getProperty = function(prop){ return prop; }
 		this.getName = function(){
-			return $getProperty.apply(db,['name']);
+			return $getProperty.apply(this,['name']);
 		};
-	}
-
+	} catch(err) {
+		trace(err);
+	}	
 };
+
 
 function Collection(_name, _objType){
 	this.name = _name;
 	this.data = [];
 	this.objType = _objType || "";
 	var coll = this;
-	if(LokiJS.debugMode)  trace('Creating collection with name [' + this.name + '] of type [' + this.objType + ']');
+
+	this.indices = [];
+
+	trace('Creating collection with name [' + this.name + '] of type [' + this.objType + ']');
 	this.add = function(obj){
 		/*
 		 * try adding object to collection
 		 */
-		if(this.objType=="" && this.data.length == 0){
+		if(coll.objType=="" && coll.data.length == 0){
 			// set object type to that of the first object added to collection
-			this.objType = obj.objType;
+			coll.objType = obj.objType;
 		} else {
 			// throw an error if the object added is not the same type as the collection's
-			if(this.objType!=obj.objType) throw 'Object type [' + obj.objType + '] is incongruent with collection type [' + this.objType +']';
-			if(this.objType=="") throw 'Object is not a model';
-			if(LokiJS.debugMode) trace('Adding object ' + obj.toString() + ' to collection ' + this.name);
+			if(coll.objType!=obj.objType) throw 'Object type [' + obj.objType + '] is incongruent with collection type [' + coll.objType +']';
+			if(coll.objType=="") throw 'Object is not a model';
+			trace('Adding object ' + obj.toString() + ' to collection ' + coll.name);
 			// add the object
-			this.data.push(obj);
+			coll.data.push(obj);
 		}
 	};
 
+	this.addMany = function(){
+		for(var i = 0; i < arguments.length; i++){
+			coll.add(arguments[i]);
+		}
+	};
 
 	var Document = function (_objType, doc){
-		if(LokiJS.debugMode) trace('_objType : ' + _objType);
-		if(LokiJS.debugMode) trace(doc);
+		trace('_objType : ' + _objType);
+		trace(doc);
 		doc.id = new Date().getTime();
 		doc.objType = _objType;
 		doc.toString = function(){ return 'Type: ' + doc.objType + ', id: ' + doc.id; };
 		return doc;
 	};
 
-	this.model = function(doc){
+	this.document = function(doc){
 		return Document(coll.objType, doc);
-	}
-}
+	};
 
-function Id(){
+	this.ensureIndex = function(property){
+		
+		if (property == null || property === undefined) throw 'Attempting to set index without an associated property';	
+		var index = {
+			name : property,
+			data : []
+		};
+		for(var i =0; i < coll.indices.length; i++){
+			if( coll.indices[i].name == property){
+				trace('Index already exists, re-indexing....');
+			}
+		}
 
-}
+		coll.indices.push(index);
+		for(var i =0; i < coll.data.length; i++){
+			index.data.push( coll.data[i][index.name] );
+		}
+		trace( coll.indices );
 
+	};
 
-function Query(queryObj){
+	this.ensureIndexAsync = function(property, callback){
+		setTimeout( function(){
+			coll.ensureIndex(property);
+			callback();
+		}, 1);
+	};
 
-}
+	this.findOne = function(prop, value){
+		var searchByIndex = false;
+		for(var i = 0; i < coll.indices.length; i++){
+			if( coll.indices[i].name == prop){
+				searchByIndex = true;
+				trace('Querying with index');
+				break;
+			}
+		}
+
+	};
+};
 
 LokiJS.trace = trace.bind(LokiJS);
-LokiJS.collection = Collection;
+loki.prototype.Collection = Collection;

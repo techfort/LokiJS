cb4573c3ed59ab9f21e4c077b8a8913a763ae487
obeliskos <admin@obeliskos.com>
2014-09-05 18:05:55 -0400

    updated examples
    added examples to show primary methods for working with resultsets and
    dynamic views

--- examples/example.js
+++ examples/example.js
@@ -111,6 +111,89 @@ window.runExample = function(){
     trace(users.find());
     trace('End findAndUpdate example');
 
+	// test chain() operations via resultset
+    sep();
+    trace('Example: Resultset chained operations');
+    // get users over 25 with substring 'in' in the name 
+    // data() ends the chain and returns data[]
+    // the where() function is a renamed equivalent to previous view() function
+    trace(
+    	users.chain()
+        	.find({'age':{'$gt': 25}})
+            .where(function(obj){ return obj.name.indexOf("in") != -1 })
+            .simplesort("age")
+            .data()
+	);
+    
+    sep();
+    trace('Example: DynamicView');
+    
+    // Create DynamicView and apply filter (these two could have been chained together)
+   	var dynView = users.addDynamicView("over30users");
+    dynView.applyFind({'age':{'$gt': 30}});
+    // .applyWhere() can also be used to apply a user supplied filter function
+
+    trace("Number of over 30 users : " + dynView.data().length);
+    lugh.age = 29;
+    users.update(lugh);
+    trace("Number of over 30 users : " + dynView.data().length);
+
+	sep();
+    trace("Example : Simple Sort");
+
+	// sort by age, true is optional second param which will sort descending
+    // we are allowed one sort which can be either simple (as below) 
+	dynView.applySimpleSort("age", true);  
+    trace(dynView.data());
+
+	sep();
+    trace("Example : Sort via compare function");
+
+	// change the sort by setting a new one in which we will supply the compareFun.
+    // sort be name ascending...
+    dynView.applySort( function(obj1, obj2) {
+    	if (obj1.name == obj2.name) return 0;
+        if (obj1.name > obj2.name) return 1;
+        if (obj1.name < obj2.name) return -1;
+    });
+    trace(dynView.data());
+    
+	sep();
+    trace("Example : Persistent DynamicView")
+    
+    // removing a dynamic view for demonstration purposes only.
+    // you can have as many dynamic views as memory allows
+    users.removeDynamicView("over30users");
+    
+    // Create a persistent dynamic view (second param indicates persistent)
+    var dynViewPersistent = users.addDynamicView("over20users", true);
+    dynViewPersistent.applyFind({'age':{'$gt': 20}});
+    dynViewPersistent.applySimpleSort("age");
+    trace(dynViewPersistent.data());
+    
+    // user should use data() but let's monitor persistent data array 
+    trace("Internal persistent data array length : " + 
+    	dynViewPersistent.resultdata.length);
+        
+    // let's also verify it matches the internal resultset filtered rows
+    trace("Internal resultset filteredrows array length : " +
+    	dynViewPersistent.resultset.filteredrows.length);
+    
+    sep();
+    trace("Example : resultset copy/forking");
+    
+    // applied filters are permanent, so use copy to fork query
+    var clonedResults = dynViewPersistent.resultset.copy();
+    clonedResults.where(function(obj){ return obj.name.indexOf("in") != -1 });
+    trace("forked query result count : " + clonedResults.data().length);
+    trace("original result count : " + dynViewPersistent.data().length);
+    
+    sep();
+    trace("Example : offset/limit");
+    trace(dynViewPersistent.resultset.copy().offset(2).limit(4).data());
+
+	sep();
+	
     db.loadJSON(json);
 
     trace(db.serialize());

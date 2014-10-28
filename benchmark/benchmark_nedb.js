c6b4dc1a9f909f82f47cb7081c35bd133e76c3c7
obeliskos <admin@obeliskos.com>
2014-10-28 18:24:13 -0400

    benchmark cleanup

--- benchmark/benchmark_nedb.js
+++ benchmark/benchmark_nedb.js
@@ -1,17 +1,15 @@
-var loki = require('../src/lokijs.js'),
-Datastore = require('nedb'),
-	//db = new loki('perftest'),
+var Datastore = require('nedb'),
   ndb = new Datastore(),
     samplecoll = null,
     arraySize = 10000,			// how large of a dataset to generate
     totalIterations = 200,	// how many times we search it
     results = [],
-	getIterations = 2000,	// get is crazy fast due to binary search so this needs separate scale
   gAsyncCount = 0,
   startTime,
   endTime,
   isIndexed = false;;
-  
+ 
+// not really using right now, if we need to time each op independently i might use this outside timing logic
 function genRandomVal()
 {
     var text = "";
@@ -23,23 +21,7 @@ function genRandomVal()
     return text;
 }
 
-function insertNext() {
-}
-
-// in addition to the loki id we will create a key of our own
-// (customId) which is number from 1- totalIterations
-// we will later perform find() queries against customId with and 
-// without an index
-
 function initializeDB() {
-	//db = new loki('perftest');
-  
-	//var start, end, totalTime;
-	//var totalTimes = [];
-	//var totalMS = 0.0;
-
-	//samplecoll = db.addCollection('samplecoll', 'samplecoll');
-    
   gAsyncCount = 0;
   
   startTime = process.hrtime();
@@ -47,7 +29,7 @@ function initializeDB() {
   // nedb uses async callbacks so we will not time each operation but 
   // use globals to count when the last async op has finished
 	for (var idx=0; idx < arraySize; idx++) {
-   	var v1 = '12345'; //genRandomVal();
+   	var v1 = '12345'; //genRandomVal(); 
     var v2 = '23456'; //genRandomVal();
     
    	ndb.insert({ 
@@ -66,53 +48,19 @@ function initializeDB() {
         testperfFind();
       }
     });
-
-		//end = process.hrtime(start);
- 		//totalTimes.push(end);
    }
-    
-	//for(var idx=0; idx < totalTimes.length; idx++) {
-	//	totalMS += totalTimes[idx][0] * 1e3 + totalTimes[idx][1]/1e6;
-	//}
-	
-    
-	//var totalMS = end[0] * 1e3 + end[1]/1e6;
-	//totalMS = totalMS.toFixed(2);
-	//var rate = arraySize * 1000 / totalMS;
 }
 
-// function testperfGet() {
-	// var start, end;
-	// var totalTimes = [];
-	// var totalMS = 0.0;
-	
-	// for (var idx=0; idx < getIterations; idx++) {
-    	// var customidx = Math.floor(Math.random() * arraySize) + 1;
-        
-		// start = process.hrtime();
-        // var results = samplecoll.get(customidx);
-		// end = process.hrtime(start);
-		// totalTimes.push(end);
-    // }
-    
-	// for(var idx=0; idx < totalTimes.length; idx++) {
-		// totalMS += totalTimes[idx][0] * 1e3 + totalTimes[idx][1]/1e6;
-	// }
-	
-	// totalMS = totalMS.toFixed(2);
-	// var rate = getIterations * 1000 / totalMS;
-	// rate = rate.toFixed(2);
-	// console.log("coll.get() : " + totalMS + "ms (" + rate + ") ops/s");
-// }
-
- function testperfFind(multiplier) {
-	// var start, end;
-	// var totalTimes = [];
-	// var totalMS = 0;
-
+// benchmark find() performance
+// called by initializedb once its last async insert is complete
+// This test runs once unindexed and on completion of all async ops it will add an index and run again
+ function testperfFind() {
 	var loopIterations = totalIterations;
-	if (typeof(multiplier) != "undefined") {
-		loopIterations = loopIterations * multiplier;
+  
+  // if running indexed, the test will complete alot faster.  
+  // Using a multiplier so that the test takes close to 1 second for more accurate rate calculation.
+	if (isIndexed) {
+		loopIterations = loopIterations * 200;
 	}
 	
   gAsyncCount = 0;
@@ -122,8 +70,6 @@ function initializeDB() {
 	for (var idx=0; idx < loopIterations; idx++) {
     var customidx = Math.floor(Math.random() * arraySize) + 1;
         
-		// start = process.hrtime();
-        //var results = samplecoll.find({ 'customId': customidx });
     ndb.find({ customId: customidx }, function (err, docs) {
       ++gAsyncCount;
       
@@ -137,112 +83,13 @@ function initializeDB() {
         if (!isIndexed) {
           isIndexed = true;
           ndb.ensureIndex({ fieldName: 'customId' }, function (err) {
-            testperfFind(200);
+            testperfFind();
           });
         }
       }
     });
-		// end = process.hrtime(start);
-		// totalTimes.push(end);
   }
-    
-	// for(var idx=0; idx < totalTimes.length; idx++) {
-		// totalMS += totalTimes[idx][0] * 1e3 + totalTimes[idx][1]/1e6;
-	// }
-	
-	// totalMS = totalMS.toFixed(2);
-	// var rate = loopIterations * 1000 / totalMS;
-	// rate = rate.toFixed(2);
-	// console.log("coll.find() : " + totalMS + "ms (" + rate + " ops/s) " + loopIterations + " iterations");
 }
-
-// function testperfRS(multiplier) {
-	// var start, end;
-	// var totalTimes = [];
-	// var totalMS = 0;
-
-	// var loopIterations = totalIterations;
-	// if (typeof(multiplier) != "undefined") {
-		// loopIterations = loopIterations * multiplier;
-	// }
-	
-	// for (var idx=0; idx < loopIterations; idx++) {
-    	// var customidx = Math.floor(Math.random() * arraySize) + 1;
-        
-		// start = process.hrtime();
-        // var results = samplecoll.chain().find({ 'customId': customidx }).data();
-		// end = process.hrtime(start)
-		// totalTimes.push(end);
-    // }
-    
-	// for(var idx=0; idx < totalTimes.length; idx++) {
-		// totalMS += totalTimes[idx][0] * 1e3 + totalTimes[idx][1]/1e6;
-	// }
-	
-	// totalMS = totalMS.toFixed(2);
-	// var rate = loopIterations * 1000 / totalMS;
-	// rate = rate.toFixed(2);
-	// console.log("resultset chained find() :  " + totalMS + "ms (" + rate + " ops/s) " + loopIterations + " iterations");
-// }
-
-// function testperfDV(multiplier) {
-	// var start, end;
-	// var start2, end2, totalTime2 = 0.0;
-	// var totalTimes = [];
-	// var totalTimes2 = [];
-	// var totalMS = 0;
-	// var totalMS2 = 0;
-    
-	// var loopIterations = totalIterations;
-	// if (typeof(multiplier) != "undefined") {
-		// loopIterations = loopIterations * multiplier;
-	// }
-	
-	// for (var idx=0; idx < loopIterations; idx++) {
-    	// var customidx = Math.floor(Math.random() * arraySize) + 1;
-       
-		// start = process.hrtime();
-		// var dv = samplecoll.addDynamicView("perfview");
-        // dv.applyFind({ 'customId': customidx });
-        // var results = dv.data();
-		// end = process.hrtime(start);
-		// totalTimes.push(end);
-      
-      	//test speed of repeated query on an already set up dynamicview
-      	// start2 = process.hrtime();
-        // var results = dv.data();
-        // end2 = process.hrtime(start2);
-		// totalTimes2.push(end2);
-        
-        // samplecoll.removeDynamicView("perfview");
-    // }
-    
-	// for(var idx=0; idx < totalTimes.length; idx++) {
-		// totalMS += totalTimes[idx][0] * 1e3 + totalTimes[idx][1]/1e6;
-		// totalMS2 += totalTimes2[idx][0] * 1e3 + totalTimes2[idx][1]/1e6;
-	// }
-	
-	// totalMS = totalMS.toFixed(2);
-	// totalMS2 = totalMS2.toFixed(2);
-	// var rate = loopIterations * 1000 / totalMS;
-	// var rate2 = loopIterations * 1000 / totalMS2;
-	// rate = rate.toFixed(2);
-	// rate2 = rate2.toFixed(2);
-	
-	// console.log("loki dynamic view first find : " + totalMS + "ms (" + rate + " ops/s) " + loopIterations + " iterations");
-	// console.log("loki dynamic view subsequent finds : " + totalMS2 + "ms (" + rate2 + " ops/s) " + loopIterations + " iterations");
-// }
 
 initializeDB();
 
-// console.log("-- Benchmarking query on non-indexed column --");
-// testperfGet();	// get bechmark on id field
-//testperfFind();	// find benchmark on unindexed customid field
-// testperfRS();	// resultset find benchmark on unindexed customid field
-// testperfDV();	// dataview find benchmarks on unindexed customid field
-
-// console.log("-- Adding binary index to query column and repeating benchmarks --");
-// samplecoll.ensureBinaryIndex("customId");
-// testperfFind(20);
-// testperfRS(15);
-// testperfDV(15);

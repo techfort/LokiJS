3a8fc179010e44752eefa2a4ac60cdc896ac8eba
Joe Minichino <joe.minichino@gmail.com>
2014-12-16 14:08:10 +0000

    fix on subclassing collection test

--- tests/testCollection.js
+++ tests/testCollection.js
@@ -1,12 +1,21 @@
 var loki = require('../src/lokijs.js'),
-	db = new loki(),
-	gordian = require('gordian'),
-	suite = new gordian('testCollection');
+  db = new loki(),
+  gordian = require('gordian'),
+  suite = new gordian('testCollection');
 
-var coll = new db.Collection('users');
+function SubclassedCollection() {
+  (new loki()).Collection.apply(this, Array.prototype.slice.call(arguments));
+}
+SubclassedCollection.prototype = new db.Collection;
+SubclassedCollection.prototype.extendedMethod = function () {
+  return this.name.toUpperCase();
+}
+var coll = new SubclassedCollection('users', {});
+
 suite.assertEqual('Exposed Collection is not null', true, coll != null);
+suite.assertEqual('Exposed Collection methods work normally', 'users'.toUpperCase(), coll.extendedMethod());
 coll.insert({
-	name: 'joe'
+  name: 'joe'
 });
 suite.assertEqual('Exposed Collection operations work normally', coll.data.length, 1);
 suite.report();

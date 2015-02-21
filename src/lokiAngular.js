1fc7c46f5a2346b56f097a200a32480d63272b90
Joe Minichino <joe.minichino@gmail.com>
2015-02-21 21:18:56 +0000

    delete window.loki in angular detector

--- src/lokiAngular.js
+++ src/lokiAngular.js
@@ -2,9 +2,20 @@
 	if (root) {
 		if (root.angular && root.loki) {
 			var module = angular.module('lokijs', [])
-				.factory('Loki', function Loki() {
+				.factory('Loki', function Loki($window) {
+					if ($window.loki) {
+						$window.thirdParty = $window.thirdParty || {};
+						$window.thirdParty.loki = root.loki;
+						try {
+							delete $window.loki;
+						} catch (err) {
+							$window.loki = undefined;
+						}
+					}
+					var loki = $window.thirdParty.loki;
 					return loki;
 				});
+
 		}
 	}
 })(window)

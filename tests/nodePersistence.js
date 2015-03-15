25e20554c7d14df3fc50bc682a20cff37f4c5884
obeliskos <phylogenesis@phylogenesiss-Mac-mini.local>
2015-03-15 10:22:40 -0400

    add autoload callback test

--- tests/nodePersistence.js
+++ tests/nodePersistence.js
@@ -16,7 +16,25 @@ var reloaded = new loki('./loki.json');
 	reloaded.loadDatabase({}, function () {
 		var users2 = reloaded.getCollection('users');
 		suite.assertEqual('There are 2 objects in the reloaded db', 2, users2.data.length);
-		suite.report();
 		require('fs').unlink('./loki.json');
 	});
 });
+
+// test autoload callback fires even when database does not exist
+function testAutoLoad() {
+	var cbSuccess = false;
+
+	var tdb = new loki('nonexistent.db', 
+	{
+        autoload: true,
+        autoloadCallback : function() { cbSuccess = true; }
+    }); 
+
+	setTimeout(function() {
+		suite.assertEqual('autoload callback was called', cbSuccess, true);
+		suite.report();
+	}, 500);
+}
+
+// due to async nature of top inline test, give it some time to complete
+setTimeout(testAutoLoad, 500);

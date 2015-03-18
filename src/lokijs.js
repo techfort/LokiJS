bf290fffc9a8a44077855ea9157bccd14776d9a5
Joe Minichino <joe.minichino@gmail.com>
2015-03-18 14:13:15 +0000

    regression on loki detection for browser

--- src/lokijs.js
+++ src/lokijs.js
@@ -12,19 +12,24 @@
     // CommonJS
     module.exports = factory();
   } else {
-
-    // Browser globals
-    var loki = factory();
+    console.log('Getting if from normal browser inclusion')
+      // Browser globals
+      //var loki = factory();
+    root.loki = factory();
+    root.thirdParty = root.thirdParty || {};
 
     if (root.loki) {
-      root.thirdParty = root.thirdParty || {};
+
       root.thirdParty.loki = root.loki;
+      /*
       try {
         delete root.loki;
       } catch (err) {
         root.loki = undefined;
       }
+      */
     }
+
   }
 }(this, function () {
 

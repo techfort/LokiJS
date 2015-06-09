8576bf0aff7101177ffd30715a73fe43a38a709c
Joe Minichino <joe.minichino@gmail.com>
2015-06-09 21:13:42 +0100

    tests pass, preparing publish

---
+++ spec/generic/persistence.spec.js
@@ -0,0 +1,33 @@
+if (typeof (window) === 'undefined') var loki = require('../../src/lokijs.js');
+
+describe('testing unique index serialization', function () {
+  var db;
+  beforeEach(function () {
+    db = new loki();
+    users = db.addCollection('users');
+    users.insert([{
+      username: 'joe'
+    }, {
+      username: 'jack'
+    }, {
+      username: 'john'
+    }, {
+      username: 'jim'
+    }]);
+    users.ensureUniqueIndex('username');
+  });
+
+  it('should not have a unique index', function () {
+    var ser = db.serialize(),
+      reloaded = new loki();
+    var loaded = reloaded.loadJSON(ser);
+    var coll = reloaded.getCollection('users');
+    expect(coll.data.length).toEqual(4);
+    expect(coll.constraints.unique.username).toBeUndefined();
+    coll.ensureUniqueIndex('username');
+    expect(coll.constraints.unique.username).toBeDefined();
+    var joe = coll.by('username', 'joe');
+    expect(joe).toBeDefined();
+    expect(joe.username).toEqual('joe');
+  });
+});

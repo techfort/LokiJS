02e438a9e30c9e5105d4247a16840ef3723ac3cf
Clay Brinlee <clay.brinlee@smpcorp.com>
2015-10-08 00:26:47 -0500

    finished most basic functions

--- src/loki-angular.js
+++ src/loki-angular.js
@@ -70,19 +70,23 @@ First set a working doc like the following:
       vm.closeDb = closeDb;
       vm.closeAllDbs = closeAllDbs;
       vm.getCollection = getCollection;
+      vm.addCollection = addCollection;
+      vm.removeCollection = removeCollection;
       vm.getDoc = getDoc;
       vm.updateDoc = updateDoc;
       vm.setCurrentDoc = setCurrentDoc;
       vm.getCurrentDoc = getCurrentDoc;
+      vm.deleteDocument = deleteDocument;
       vm.deleteCurrentDoc = deleteCurrentDoc;
       vm.deleteDatabase = deleteDatbase;
       vm.addDocument = addDocument;
       vm.insertItemInDoc = insertItemInDoc;
       var currentDoc = {};
+      var currentColl = {};
       numOfJsonDatabases = getNumberOfJsonDatabases();
       
   
-      
+           
       function getCurrentDoc(){
         return currentDoc;
       }
@@ -90,6 +94,18 @@ First set a working doc like the following:
       function deleteDatbase(data){
         localStorage.removeItem(data);
       }
+      
+      function deleteDocument(dbName, collName, doc){  //doc should be in {name:value} format 
+        return $q(function(resolve, reject){ 
+          userDbPreference = dbName;
+            _getem('delete_doc', dbName, collName, "", "", doc)
+            .then(function(data){
+              currentDoc = {};
+              resolve(data);
+            })
+         });
+      }
+
       
       function insertItemInDoc(item){
         return $q(function(resolve, reject){ 
@@ -113,8 +129,9 @@ First set a working doc like the following:
         });
       }
       
-      function addDocument(dbName, collName, newDoc){
+      function addDocument(dbName, collName, newDoc){        
         return $q(function(resolve, reject){ 
+          userDbPreference = dbName;
             _getem('create_doc', dbName, collName, "", "", newDoc)
             .then(function(data){
               currentDoc.dbName = dbName;
@@ -122,7 +139,7 @@ First set a working doc like the following:
               currentDoc.doc = data;
               currentDoc.lokiNum = data[0].$loki;
               resolve(data);
-            });
+            })
          });
       }
       
@@ -136,7 +153,7 @@ First set a working doc like the following:
               currentDoc.doc = data;
               currentDoc.lokiNum = data[0].$loki;
               resolve(data);
-            });
+            })
          });
       }
       
@@ -146,7 +163,7 @@ First set a working doc like the following:
            _getem('update_doc', currentDoc.dbName, currentDoc.collName, currentDoc.doc, thekey, thevalue)
             .then(function(data){
               resolve(data);
-            });
+            })
            }
            else {
              reject("you have to set a current doc first, use: setCurrentDoc(dbName, collName, docName)");
@@ -164,18 +181,47 @@ First set a working doc like the following:
               currentDoc.doc = data;
               currentDoc.lokiNum = data[0].$loki;
               resolve(data);
-            });
+            })
          });
       }
       
       function getCollection(dbName, collName){
-         return $q(function(resolve, reject){ 
-            _getem(dbName, collName)
+         return $q(function(resolve, reject){
+           userDbPreference = dbName;   
+            _getem('get_collection', dbName, collName)
             .then(function(data){
+              currentColl.dbName = dbName;
+              currentColl.collName = collName;
               resolve(data);
-            });
+            })
          });
       }
+      
+      function removeCollection(dbName, collName){
+         return $q(function(resolve, reject){
+           userDbPreference = dbName;   
+            _getem('remove_collection', dbName, collName)
+            .then(function(data){
+              currentColl = {};
+              resolve(data);
+            })
+         });
+      }
+      
+      function addCollection(collData){
+         return $q(function(resolve, reject){
+           userDbPreference = collData.db;   
+            _getem('add_collection', collData.db, '','', '', collData)
+            .then(function(data){
+              currentColl.dbName = collData.db;
+              currentColl.collName = collData.collection;
+              resolve(data);
+            })
+         });
+      }
+      
+      
+      
       
       function _getem(operation, dbName, collName, docName, thekey, thevalue){
         return $q(function(resolve, reject){ 
@@ -208,6 +254,7 @@ First set a working doc like the following:
           }
           function getdata(){ 
               if(operation === 'update_doc' || operation === 'insert_item_in_doc'){
+                db.loadDatabase(dbName);
                 var coll = db.getCollection(collName);
                   found = coll.get(parseInt(currentDoc.lokiNum, 10));
                   
@@ -227,15 +274,35 @@ First set a working doc like the following:
                   resolve(true);
               }
               else  if (operation === 'get_doc' || operation === 'set_doc'){
+                  db.loadDatabase(dbName);
                   coll = db.getCollection(collName);
                   var found = coll.find(docName);
                   resolve(angular.fromJson(found));
               } 
               else if(operation === 'get_collection'){
+                db.loadDatabase(dbName);
                 coll = db.getCollection(collName);
                 resolve(angular.fromJson(coll)); 
               }
-              else if(operation === 'create_doc'){
+              else if(operation === 'remove_collection'){
+                db.loadDatabase(dbName);
+                db.removeCollection(collName);
+                //coll = db.getCollection(collName);
+                db.save(function(){
+                  resolve('collection deleted');
+                });                
+              }
+              else if(operation === 'add_collection'){               
+                    db.loadDatabase(thevalue.db);
+                    var items = db.addCollection(thevalue.collection);
+                    items.insert(thevalue.documents);
+                    db.save(function(){
+                      resolve('collection added');
+                    });  
+                    
+              }
+              else if(operation === 'create_doc'){   
+                db.loadDatabase(dbName);             
                 coll = db.getCollection(collName);
                 coll.insert(thevalue);
                 db.save(function(){
@@ -244,6 +311,18 @@ First set a working doc like the following:
                 });
                  
               }
+              else if(operation === 'delete_doc'){
+                db.loadDatabase(dbName);
+                coll = db.getCollection(collName);
+                var getid = coll.find(thevalue);
+                var id = getid[0].$loki;                
+                coll.remove(id);
+                db.save(function(){                  
+                  resolve('deleted');
+                });
+                 
+              }
+               // _getem('delete_doc', dbName, collName, "", "", doc)
               else if(operation === 'delete_current_doc'){
                 coll = db.getCollection(currentDoc.collName);
                 if (!coll){
@@ -321,7 +400,7 @@ First set a working doc like the following:
             });
           }
           else {
-            console.log('db list already initialized');
+            console.log('db list already initialized')
             resolve();
           }
         });
@@ -360,7 +439,7 @@ First set a working doc like the following:
         var item = {};
         item.filename = currentdb.db;
         item.json = userPrefJsonFile;
-        dbitems.push(item);
+        dbitems.push(item);;
         resolve();
         });
        });
@@ -382,6 +461,7 @@ First set a working doc like the following:
       
       var still_running = false;
       var current_iteration = 1;
+      
       function initialiseAll(){
          return $q(function(resolve, reject){
            initialiseDbList()

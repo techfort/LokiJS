345a21ec43a1f1c943c2cabdc0dda31bcc66675b
VladimirTechMan <VladimirTechMan@gmail.com>
2016-02-03 17:09:21 +0300

    Avoid possible clashes with sorting. There is a better place for the improvement proposed.

--- src/lokijs.js
+++ src/lokijs.js
@@ -2193,11 +2193,6 @@
         }
       }
       else {
-        // if the filter references all the collection documents, just copy the document array
-        if (len === data.length) {
-          return data.slice();
-        }
-
         for (i = 0; i < len; i++) {
           result.push(data[fr[i]]);
         }

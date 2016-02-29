9b8109b37facac231ed8ce7ae595a9f280ed6bed
Alexander Nestorov <alexandernst@gmail.com>
2016-02-29 01:09:17 +0100

    Fix corner case for glHelper

--- src/lokijs.js
+++ src/lokijs.js
@@ -137,7 +137,7 @@
         }
 
         if (prop1 === undefined || prop1 === null || prop1 === false || prop2 === true) {
-          return false;
+          return !!equal || false;
         }
         if (prop2 === undefined || prop2 === null || prop1 === true || prop2 === false) {
           return true;

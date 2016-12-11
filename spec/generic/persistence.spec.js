19c6389837e8b120ab4813a015d0b5a9936b62d3
obeliskos <admin@obeliskos.com>
2016-12-11 11:25:30 -0500

    #498 : Added 'paging' options to LokiPartitioningAdapter for Chrome
    Chrome indexeddb seems to have limit on how big a single record can
    be... somewhere around 30-60megs (possibly unicode doubled).  To
    accomodate that I allow individual collections to span multiple save
    slots.  Will rework wiki within next week to give better
    instruction/examples.

--- spec/generic/persistence.spec.js
+++ spec/generic/persistence.spec.js
@@ -304,6 +304,74 @@ describe('testing adapter functionality', function () {
     expect(db2.getCollection("another").findOne({ a: 1}).b).toEqual(3);
   });
 
+  it('verify partioning adapter with paging mode enabled works', function() {
+    var mem = new loki.LokiMemoryAdapter();
+
+    // we will use an exceptionally low page size (128bytes) to test with small dataset
+    var adapter = new loki.LokiPartitioningAdapter(mem, { paging: true, pageSize: 128});
+
+    var db = new loki('sandbox.db', {adapter: adapter});
+
+    // Add a collection to the database
+    var items = db.addCollection('items');
+    items.insert({ name : 'mjolnir', owner: 'thor', maker: 'dwarves' });
+    items.insert({ name : 'gungnir', owner: 'odin', maker: 'elves' });
+    var tyr = items.insert({ name : 'tyrfing', owner: 'Svafrlami', maker: 'dwarves' });
+    items.insert({ name : 'draupnir', owner: 'odin', maker: 'elves' });
+
+    var another = db.addCollection('another');
+    var ai = another.insert({ a:1, b:2 });
+
+    // for purposes of our memory adapter it is pretty much synchronous
+    db.saveDatabase();
+
+    // should have partitioned the data
+    expect(Object.keys(mem.hashStore).length).toEqual(4);
+    expect(mem.hashStore.hasOwnProperty("sandbox.db")).toEqual(true);
+    expect(mem.hashStore.hasOwnProperty("sandbox.db.0.0")).toEqual(true);
+    expect(mem.hashStore.hasOwnProperty("sandbox.db.0.1")).toEqual(true);
+    expect(mem.hashStore.hasOwnProperty("sandbox.db.1.0")).toEqual(true);
+    // all partitions should have been saved once each
+    expect(mem.hashStore["sandbox.db"].savecount).toEqual(1);
+    expect(mem.hashStore["sandbox.db.0.0"].savecount).toEqual(1);
+    expect(mem.hashStore["sandbox.db.0.1"].savecount).toEqual(1);
+    expect(mem.hashStore["sandbox.db.1.0"].savecount).toEqual(1);
+
+    // so let's go ahead and update one of our collections to make it dirty
+    ai.b = 3;
+    another.update(ai);
+
+    // and save again to ensure lastsave is different on for db container and that one collection
+    db.saveDatabase();
+
+    // db container always gets saved since we currently have no 'dirty' flag on it to check
+    expect(mem.hashStore["sandbox.db"].savecount).toEqual(2);
+    // we didn't change this
+    expect(mem.hashStore["sandbox.db.0.0"].savecount).toEqual(1);
+    expect(mem.hashStore["sandbox.db.0.0"].savecount).toEqual(1);
+    // we updated this collection so it should have been saved again
+    expect(mem.hashStore["sandbox.db.1.0"].savecount).toEqual(2);
+
+    // now update a multi page items collection and verify both pages were saved
+    tyr.maker = "elves";
+    items.update(tyr);
+    db.saveDatabase();
+    expect(mem.hashStore["sandbox.db"].savecount).toEqual(3);
+    expect(mem.hashStore["sandbox.db.0.0"].savecount).toEqual(2);
+    expect(mem.hashStore["sandbox.db.0.0"].savecount).toEqual(2);
+    expect(mem.hashStore["sandbox.db.1.0"].savecount).toEqual(2);
+
+    // ok now lets load from it
+    var db2 = new loki('sandbox.db', { adapter: adapter});
+    db2.loadDatabase();
+
+    expect(db2.collections.length).toEqual(2);
+    expect(db2.collections[0].data.length).toEqual(4);
+    expect(db2.collections[1].data.length).toEqual(1);
+    expect(db2.getCollection("items").findOne({ name : 'tyrfing'}).maker).toEqual("elves");
+    expect(db2.getCollection("another").findOne({ a: 1}).b).toEqual(3);
+  });
+
   it('verify reference adapters get db reference which is copy and serializable-safe', function() {
     // Current loki functionality with regards to reference mode adapters:
     // Since we don't use serializeReplacer on reference mode adapters, we make 
--- src/lokijs.js
+++ src/lokijs.js
@@ -1639,17 +1639,29 @@
     /**
      * An adapter for adapters.  Converts a non reference mode adapter into a reference mode adapter
      * which can perform destructuring and partioning.  Each collection will be stored in its own key/save and
-     * only dirty collections will be saved.
+     * only dirty collections will be saved.  If you  turn on paging with default page size of 25megs and save
+     * a 75 meg collection it should use up roughly 3 save slots (key/value pairs sent to inner adapter). 
+     * A dirty collection that spans three pages will save all three pages again
+     * Paging mode was added mainly because Chrome has issues saving 'too large' of a string within a 
+     * single indexeddb row.  If a single document update causes the collection to be flagged as dirty, all
+     * of that collection's pages will be written on next save.
      *
      * @param {object} adapter - reference to a 'non-reference' mode loki adapter instance.
+     * @param {object=} options - configuration options for partitioning and paging
+     * @param {bool} options.paging - (default: false) set to true to enable paging collection data.
+     * @param {int} options.pageSize - (default : 25MB) you can use this to limit size of strings passed to inner adapter.
      * @constructor LokiPartitioningAdapter
      */
-    function LokiPartitioningAdapter(adapter) {
+    function LokiPartitioningAdapter(adapter, options) {
       this.mode = "reference";
       this.adapter = null;
+      this.options = options || {};
+      this.dbref = null;
+      this.dbname = "";
+      this.pageIterator = {};
 
+      // verify user passed an appropriate adapter
       if (adapter) {
-        // reference mode adapters generally examine the structure of an entire loki database so our 'parts' would not work
         if (adapter.mode === "reference") {
           throw new Error("LokiPartitioningAdapter cannot be instantiated with a reference mode adapter");
         }
@@ -1659,6 +1671,20 @@
       }
       else {
         throw new Error("LokiPartitioningAdapter requires a (non-reference mode) adapter on construction");
+      }
+
+      // set collection paging defaults
+      if (!this.options.hasOwnProperty("paging")) {
+        this.options.paging = false;
+      }
+
+      // default to page size of 25 megs (can be up to your largest serialized object size larger than this)
+      if (!this.options.hasOwnProperty("pageSize")) {
+        this.options.pageSize = 25*1024*1024;
+      }
+
+      if (!this.options.hasOwnProperty("delimiter")) {
+        this.options.delimiter = '$<\n';
       }
     }
 
@@ -1672,6 +1698,8 @@
      */
     LokiPartitioningAdapter.prototype.loadDatabase = function (dbname, callback) {
       var self=this;
+      this.dbname = dbname;
+      this.dbref = new Loki(dbname);
 
       // load the db container (without data)
       this.adapter.loadDatabase(dbname, function(result) {
@@ -1681,19 +1709,23 @@
 
         // I will want to use loki destructuring helper methods so i will inflate into typed instance
         var db = JSON.parse(result);
-        var dbref = new Loki(dbname);
-        dbref.loadJSONObject(db);
+        self.dbref.loadJSONObject(db);
         db = null;
 
-        var clen = dbref.collections.length;
+        var clen = self.dbref.collections.length;
 
-        if (dbref.collections.length === 0) {
-          callback(dbref);
+        if (self.dbref.collections.length === 0) {
+          callback(self.dbref);
           return;
         }
 
-        self.loadNextPartition(dbname, dbref, 0, function() {
-          callback(dbref);
+        self.pageIterator = {
+          collection: 0,
+          pageIndex: 0
+        };
+
+        self.loadNextPartition(0, function() {
+          callback(self.dbref);
         });
       });
     };
@@ -1701,24 +1733,77 @@
     /**
      * Used to sequentially load each collection partition, one at a time.
      *
-     * @param {string} dbname - name of the database (filename/keyname)
-     * @param {object} db - reference to internally database instance we are reconstructing.
      * @param {int} partition - ordinal collection position to load next
      * @param {function} callback - adapter callback to return load result to caller
      */
-    LokiPartitioningAdapter.prototype.loadNextPartition = function(dbname, db, partition, callback) {
-      var keyname = dbname + "." + partition;
+    LokiPartitioningAdapter.prototype.loadNextPartition = function(partition, callback) {
+      var keyname = this.dbname + "." + partition;
       var self=this;
 
-      this.adapter.loadDatabase(keyname, function(result) {
-        var data = db.deserializeCollection(result, { delimited: true, collectionIndex: partition });
-        db.collections[partition].data = data;
+      if (this.options.paging === true) {
+        this.pageIterator.pageIndex = 0;
+        this.loadNextPage(callback);
+        return;
+      }
 
-        if (++partition < db.collections.length) {
-          self.loadNextPartition(dbname, db, partition, callback);
+      this.adapter.loadDatabase(keyname, function(result) {
+        var data = self.dbref.deserializeCollection(result, { delimited: true, collectionIndex: partition });
+        self.dbref.collections[partition].data = data;
+
+        if (++partition < self.dbref.collections.length) {
+          self.loadNextPartition(partition, callback);
         }
         else {
           callback();
+        }
+      });
+    };
+
+    /**
+     * Used to sequentially load the next page of collection partition, one at a time.
+     *
+     * @param {function} callback - adapter callback to return load result to caller
+     */
+    LokiPartitioningAdapter.prototype.loadNextPage = function(callback) {
+      // calculate name for next saved page in sequence
+      var keyname = this.dbname + "." + this.pageIterator.collection + "." + this.pageIterator.pageIndex;
+      var self=this;
+
+      // load whatever page is next in sequence
+      this.adapter.loadDatabase(keyname, function(result) {
+        var data = result.split(self.options.delimiter);
+        result = ""; // free up memory now that we have split it into array
+        var dlen = data.length;
+        var idx;
+
+        // detect if last page by presence of final empty string element and remove it if so
+        var isLastPage = (data[dlen-1] === "");
+        if (isLastPage) {
+          data.pop();
+          dlen = data.length;
+        }
+
+        // convert stringified array elements to object instances and push to collection data
+        for(idx=0; idx < dlen; idx++) {
+          self.dbref.collections[self.pageIterator.collection].data.push(JSON.parse(data[idx]));
+          data[idx] = null;
+        }
+        data = [];
+
+        // if last page, we are done with this partition
+        if (isLastPage) {
+
+          // if there are more partitions, kick off next partition load
+          if (++self.pageIterator.collection < self.dbref.collections.length) {
+            self.loadNextPartition(self.pageIterator.collection, callback);
+          }
+          else {
+            callback();
+          }
+        }
+        else {
+          self.pageIterator.pageIndex++;
+          self.loadNextPage(callback);
         }
       });
     };
@@ -1736,16 +1821,19 @@
     LokiPartitioningAdapter.prototype.exportDatabase = function(dbname, dbref, callback) {
       var self=this;
       var idx, clen = dbref.collections.length;
-      var dirtyPartitions = [-1];
+
+      this.dbref = dbref;
+      this.dbname = dbname;
 
       // queue up dirty partitions to be saved
+      this.dirtyPartitions = [-1];
       for(idx=0; idx<clen; idx++) {
         if (dbref.collections[idx].dirty) {
-          dirtyPartitions.push(idx);
+          this.dirtyPartitions.push(idx);
         }
       }
 
-      this.saveNextPartition(dbname, dbref, dirtyPartitions, function(err) {
+      this.saveNextPartition(function(err) {
         callback(err);
       });
     };
@@ -1753,17 +1841,35 @@
     /**
      * Helper method used internally to save each dirty collection, one at a time.
      *
-     * @param {string} dbname - name of the database (filename/keyname)
-     * @param {object} dbref - reference to internally database instance we are reconstructing.
-     * @param {array} dirtyPartitions - array of partition numbers which need to be saved
      * @param {function} callback - adapter callback to return load result to caller
      */
-    LokiPartitioningAdapter.prototype.saveNextPartition = function(dbname, dbref, dirtyPartitions, callback) {
+    LokiPartitioningAdapter.prototype.saveNextPartition = function(callback) {
       var self=this;
-      var partition = dirtyPartitions.shift();
-      var keyname = dbname + ((partition===-1)?"":("." + partition));
+      var partition = this.dirtyPartitions.shift();
+      var keyname = this.dbname + ((partition===-1)?"":("." + partition));
 
-      var result = dbref.serializeDestructured({
+      // if we are doing paging and this is collection partition
+      if (this.options.paging && partition !== -1) {
+        this.pageIterator = {
+          collection: partition,
+          docIndex: 0,
+          pageIndex: 0
+        };
+
+        // since saveNextPage recursively calls itself until done, our callback means this whole paged partition is finished
+        this.saveNextPage(function(err) {
+          if (self.dirtyPartitions.length === 0) {
+            callback(err);
+          }
+          else {
+            self.saveNextPartition(callback);
+          }
+        });
+        return;
+      }
+
+      // otherwise this is 'non-paged' partioning...
+      var result = this.dbref.serializeDestructured({
         partitioned : true,
         delimited: true,
         partition: partition
@@ -1774,14 +1880,74 @@
           callback(err);
           return;
         }
-        
-        if (dirtyPartitions.length === 0) {
+
+        if (self.dirtyPartitions.length === 0) {
           callback(null);
         }
         else {
-          self.saveNextPartition(dbname, dbref, dirtyPartitions, callback);
+          self.saveNextPartition(callback);
         }
       });
+    };
+
+    /**
+     * Helper method used internally to generate and save the next page of the current (dirty) partition.
+     *
+     * @param {function} callback - adapter callback to return load result to caller
+     */
+    LokiPartitioningAdapter.prototype.saveNextPage = function(callback) {
+      var self=this;
+      var coll = this.dbref.collections[this.pageIterator.collection];
+      var keyname = this.dbname + "." + this.pageIterator.collection + "." + this.pageIterator.pageIndex;
+      var pageLen=0,
+        cdlen = coll.data.length,
+        delimlen = this.options.delimiter.length;
+      var serializedObject = "",
+        pageBuilder = "";
+      var doneWithPartition=false,
+        doneWithPage=false;
+
+      var pageSaveCallback = function(err) {
+        pageBuilder = "";
+
+        if (err) {
+          callback(err);
+        }
+
+        // update meta properties then continue process by invoking callback
+        if (doneWithPartition) {
+          callback(null);
+        }
+        else {
+          self.pageIterator.pageIndex++;
+          self.saveNextPage(callback);
+        }
+      };
+
+      while (true) {
+        // serialize object
+        serializedObject = JSON.stringify(coll.data[this.pageIterator.docIndex]);
+        pageBuilder += serializedObject;
+        pageLen += serializedObject.length;
+
+        // if no more documents in collection to add, we are done with partition
+        if (++this.pageIterator.docIndex >= cdlen) doneWithPartition = true;
+        // if our current page is bigger than defined pageSize, we are done with page
+        if (pageLen >= this.options.pageSize) doneWithPage = true;
+
+        // if not done with current page, need delimiter before next item
+        // if done with partition we also want a delmiter to indicate 'end of pages' final empty row
+        if (!doneWithPage || doneWithPartition) {
+          pageBuilder += this.options.delimiter;
+          pageLen += delimlen;
+        }
+
+        // if we are done with page save it and pass off to next recursive call or callback
+        if (doneWithPartition || doneWithPage) {
+          this.adapter.saveDatabase(keyname, pageBuilder, pageSaveCallback);
+          return;
+        }
+      }
     };
 
     /**

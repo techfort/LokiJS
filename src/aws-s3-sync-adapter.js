aea51d2c2a83758642285c569d963bf4477501e4
Simon <hutch120@gmail.com>
2018-04-30 16:19:48 +1000

    Added semicolons for linter

--- src/aws-s3-sync-adapter.js
+++ src/aws-s3-sync-adapter.js
@@ -18,7 +18,7 @@
        'secretAccessKey': process.env.secretAccessKey, // Avoid hardcoding credentials
        'bucket': '...'
      }
-     const adapter = new AWSS3SyncAdapter(options)
+     const adapter = new AWSS3SyncAdapter(options);
      db = new Loki('somefile.json', {
        autoload: false,
        autosave: true,
@@ -63,24 +63,24 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD
-    define([], factory)
+    define([], factory);
   } else if (typeof exports === 'object') {
     // CommonJS
-    module.exports = factory()
+    module.exports = factory();
   } else {
     // Browser globals
-    root.lokiAWSS3SyncAdapter = factory()
+    root.lokiAWSS3SyncAdapter = factory();
   }
 }(this, function () {
   return (function (options) {
-    'use strict'
+    'use strict';
 
     function AWSS3SyncAdapterError (message) {
-      this.name = 'AWSS3SyncAdapterError'
-      this.message = (message || '')
+      this.name = 'AWSS3SyncAdapterError';
+      this.message = (message || '');
     }
 
-    AWSS3SyncAdapterError.prototype = Error.prototype
+    AWSS3SyncAdapterError.prototype = Error.prototype;
 
     /**
      * This adapter requires an object options is passed containing the following properties:
@@ -91,36 +91,36 @@
      */
 
     function AWSS3SyncAdapter (options) {
-      this.options = options
+      this.options = options;
 
       if (!options) {
-        throw new AWSS3SyncAdapterError('No options configured in AWSS3SyncAdapter')
+        throw new AWSS3SyncAdapterError('No options configured in AWSS3SyncAdapter');
       }
 
       if (!options.AWS) {
-        throw new AWSS3SyncAdapterError('No AWS library specified in options')
+        throw new AWSS3SyncAdapterError('No AWS library specified in options');
       }
 
       if (!options.accessKeyId) {
-        throw new AWSS3SyncAdapterError('No accessKeyId property specified in options')
+        throw new AWSS3SyncAdapterError('No accessKeyId property specified in options');
       }
 
       if (!options.secretAccessKey) {
-        throw new AWSS3SyncAdapterError('No secretAccessKey property specified in options')
+        throw new AWSS3SyncAdapterError('No secretAccessKey property specified in options');
       }
 
       if (!options.bucket) {
-        throw new AWSS3SyncAdapterError('No bucket property specified in options')
+        throw new AWSS3SyncAdapterError('No bucket property specified in options');
       }
 
-      this.options.AWS.config.update({accessKeyId: options.accessKeyId, secretAccessKey: options.secretAccessKey})
+      this.options.AWS.config.update({accessKeyId: options.accessKeyId, secretAccessKey: options.secretAccessKey});
     }
 
     AWSS3SyncAdapter.prototype.saveDatabase = function (name, data, callback) {
-      console.log('AWSS3SyncAdapter.prototype.saveDatabase():', name)
+      console.log('AWSS3SyncAdapter.prototype.saveDatabase():', name);
 
-      const s3 = new this.options.AWS.S3()
-      const base64str = Buffer.from(data).toString('base64')
+      const s3 = new this.options.AWS.S3();
+      const base64str = Buffer.from(data).toString('base64');
 
       var params = {
         Body: base64str,
@@ -128,48 +128,48 @@
         Key: name,
         ServerSideEncryption: 'AES256',
         Tagging: '' // key1=value1&key2=value2
-      }
+      };
 
       s3.putObject(params, function (err, data) {
         if (err) {
-          console.log('AWSS3SyncAdapter.prototype.saveDatabase() Error:', err, err.stack)
-          throw new AWSS3SyncAdapterError('Remote sync failed')
+          console.log('AWSS3SyncAdapter.prototype.saveDatabase() Error:', err, err.stack);
+          throw new AWSS3SyncAdapterError('Remote sync failed');
         } else {
-          return callback(null, data)
+          return callback(null, data);
         }
-      })
-    }
+      });
+    };
 
     AWSS3SyncAdapter.prototype.loadDatabase = function (name, callback) {
-      console.log('AWSS3SyncAdapter.prototype.loadDatabase():', name)
-      const s3 = new this.options.AWS.S3()
+      console.log('AWSS3SyncAdapter.prototype.loadDatabase():', name);
+      const s3 = new this.options.AWS.S3();
 
       var params = {
         Bucket: this.options.bucket,
         Key: name
-      }
+      };
 
       s3.getObject(params, function (err, data) {
         if (err) {
-          console.log('AWSS3SyncAdapter.prototype.loadDatabase() Error:', err, err.stack)
-          throw new AWSS3SyncAdapterError('Remote load failed')
+          console.log('AWSS3SyncAdapter.prototype.loadDatabase() Error:', err, err.stack);
+          throw new AWSS3SyncAdapterError('Remote load failed');
         } else {
           if (!data.Body) {
-            throw new AWSS3SyncAdapterError('Remote load failed, no data found')
+            throw new AWSS3SyncAdapterError('Remote load failed, no data found');
           }
           try {
-            var base64data = data.Body.toString()
-            var asciiData = Buffer.from(base64data, 'base64').toString()
-            var lokiDBObj = JSON.parse(asciiData) // For json objects
-            console.log('AWSS3SyncAdapter.prototype.loadDatabase() asciiData:', lokiDBObj)
-            return callback(lokiDBObj)
+            var base64data = data.Body.toString();
+            var asciiData = Buffer.from(base64data, 'base64').toString();
+            var lokiDBObj = JSON.parse(asciiData); // For json objects
+            console.log('AWSS3SyncAdapter.prototype.loadDatabase() asciiData:', lokiDBObj);
+            return callback(lokiDBObj);
           } catch (e) {
-            throw new AWSS3SyncAdapterError('Remote load failed, invalid loki database')
+            throw new AWSS3SyncAdapterError('Remote load failed, invalid loki database');
           }
         }
-      })
-    }
+      });
+    };
 
-    return AWSS3SyncAdapter
-  }())
-}))
+    return AWSS3SyncAdapter;
+  }());
+}));

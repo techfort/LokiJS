084fc9f07d4253eb74966d12b89b3287c612555d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-09-27 10:09:23 +0200

    Change dotSubScan(): if nested object does not exist, use 'undefined' for matching

--- src/lokijs.js
+++ src/lokijs.js
@@ -335,14 +335,12 @@
     function dotSubScan(root, paths, fun, value, poffset) {
       var pathOffset = poffset || 0;
       var path = paths[pathOffset];
-      if (root === undefined || root === null || !hasOwnProperty.call(root, path)) {
-        if (pathOffset + 1 < paths.length) {
-          return false;
-        }
-      }
 
       var valueFound = false;
-      var element = root[path];
+      var element;
+      if (typeof root === 'object' && path in root) {
+        element = root[path];
+      }
       if (pathOffset + 1 >= paths.length) {
         // if we have already expanded out the dot notation,
         // then just evaluate the test function and value on the element

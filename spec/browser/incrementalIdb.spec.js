cc740d336e194273231ebd268de17bab1f4c67b8
Christopher Fu <invent288cfu@gmail.com>
2023-05-29 14:32:04 -0400

    build: updating build tools to use headlesschrome, tsc, terser

--- spec/browser/incrementalIdb.spec.js
+++ spec/browser/incrementalIdb.spec.js
@@ -1,34 +1,36 @@
-describe('IncrementalIndexedDBAdapter', function () {
-  it('initializes Loki properly', function() {
-    var adapter = new IncrementalIndexedDBAdapter('tests');
-    var db = new loki('test.db', { adapter: adapter });
-    var coll = db.addCollection('coll');
+describe("IncrementalIndexedDBAdapter", function () {
+  it("initializes Loki properly", function () {
+    var adapter = new IncrementalIndexedDBAdapter("tests");
+    var db = new loki("test.db", { adapter: adapter });
+    var coll = db.addCollection("coll");
 
     expect(db.isIncremental).toBe(true);
     expect(coll.isIncremental).toBe(true);
     expect(adapter.chunkSize).toBe(100);
-    expect(adapter.mode).toBe('incremental');
-  })
+    expect(adapter.mode).toBe("incremental");
+  });
   function checkDatabaseCopyIntegrity(source, copy) {
-    source.collections.forEach(function(sourceCol, i) {
+    source.collections.forEach(function (sourceCol, i) {
       var copyCol = copy.collections[i];
       expect(copyCol.name).toBe(sourceCol.name);
       expect(copyCol.data.length).toBe(sourceCol.data.length);
 
-      copyCol.data.every(function(copyEl, elIndex) {
-        expect(JSON.stringify(copyEl)).toBe(JSON.stringify(source.collections[i].data[elIndex]))
-      })
-    })
+      copyCol.data.every(function (copyEl, elIndex) {
+        expect(JSON.stringify(copyEl)).toBe(
+          JSON.stringify(source.collections[i].data[elIndex])
+        );
+      });
+    });
   }
-  it('checkDatabaseCopyIntegrity works', function() {
-    var db = new loki('test.db');
-    var col1 = db.addCollection('test_collection');
+  it("checkDatabaseCopyIntegrity works", function () {
+    var db = new loki("test.db");
+    var col1 = db.addCollection("test_collection");
 
-    var doc1 = { foo: '1' };
-    var doc2 = { foo: '2' };
-    var doc3 = { foo: '3' };
+    var doc1 = { foo: "1" };
+    var doc2 = { foo: "2" };
+    var doc3 = { foo: "3" };
     col1.insert([doc1, doc2, doc3]);
-    doc2.bar = 'true';
+    doc2.bar = "true";
     col1.update(doc2);
     col1.remove(doc3);
 
@@ -43,7 +45,7 @@ describe('IncrementalIndexedDBAdapter', function () {
     //   copy.collections[0].data.push({ hello: '!'})
     //   checkDatabaseCopyIntegrity(db, copy);
     // }).toThrow();
-  })
+  });
   // it('basically works', function(done) {
   //   var adapter = new IncrementalIndexedDBAdapter('tests');
   //   var db = new loki('test.db', { adapter: adapter });
@@ -89,18 +91,18 @@ describe('IncrementalIndexedDBAdapter', function () {
   // it('stores data in the expected format', function() {
   // })
   // NOTE: Because PhantomJS doesn't support IndexedDB, I moved tests to spec/incrementalidb.html
-  it('handles dirtyIds during save properly', function() {
-    var adapter = new IncrementalIndexedDBAdapter('tests');
-    var db = new loki('test.db', { adapter: adapter });
-    var col1 = db.addCollection('test_collection');
-    var col2 = db.addCollection('test_collection2');
-    col2.dirty = false
+  it("handles dirtyIds during save properly", function () {
+    var adapter = new IncrementalIndexedDBAdapter("tests");
+    var db = new loki("test.db", { adapter: adapter });
+    var col1 = db.addCollection("test_collection");
+    var col2 = db.addCollection("test_collection2");
+    col2.dirty = false;
 
-    var doc1 = { foo: '1' };
-    var doc2 = { foo: '2' };
-    var doc3 = { foo: '3' };
+    var doc1 = { foo: "1" };
+    var doc2 = { foo: "2" };
+    var doc3 = { foo: "3" };
     col1.insert([doc1, doc2, doc3]);
-    doc2.bar = 'true';
+    doc2.bar = "true";
     col1.update(doc2);
     col1.remove(doc3);
 
@@ -111,7 +113,7 @@ describe('IncrementalIndexedDBAdapter', function () {
 
     // simulate save - don't go through IDB, just check that logic is good
     var callCallback;
-    adapter.saveDatabase = function(dbname, getLokiCopy, callback) {
+    adapter.saveDatabase = function (dbname, getLokiCopy, callback) {
       getLokiCopy();
       callCallback = callback;
     };
@@ -120,21 +122,21 @@ describe('IncrementalIndexedDBAdapter', function () {
     db.saveDatabase();
     expect(col1.dirtyIds).toEqual([]);
     expect(col1.dirty).toBe(false);
-    callCallback(new Error('foo'));
+    callCallback(new Error("foo"));
     expect(col1.dirtyIds).toEqual(dirty);
     expect(col1.dirty).toBe(true);
     expect(col2.dirty).toBe(false);
 
     // new dirtied ids get added in case of rollback
     db.saveDatabase();
-    var doc4 = { foo: '4' };
+    var doc4 = { foo: "4" };
     col1.insert(doc4);
     expect(col1.dirtyIds).toEqual([doc4.$loki]);
-    var doc5 = { foo: '5' };
+    var doc5 = { foo: "5" };
     col2.insert(doc5);
     expect(col2.dirty).toBe(true);
     expect(col2.dirtyIds).toEqual([doc5.$loki]);
-    callCallback(new Error('foo'));
+    callCallback(new Error("foo"));
     expect(col1.dirtyIds).toEqual([doc4.$loki].concat(dirty));
     expect(col1.dirty).toBe(true);
     expect(col2.dirtyIds).toEqual([doc5.$loki]);
@@ -143,10 +145,10 @@ describe('IncrementalIndexedDBAdapter', function () {
     // if successful, dirty ids don't zero out
     db.saveDatabase();
     expect(col1.dirtyIds).toEqual([]);
-    var doc6 = { foo: '6' };
+    var doc6 = { foo: "6" };
     col1.insert(doc6);
     expect(col1.dirtyIds).toEqual([doc6.$loki]);
     callCallback();
     expect(col1.dirtyIds).toEqual([doc6.$loki]);
-  })
-})
+  });
+});
